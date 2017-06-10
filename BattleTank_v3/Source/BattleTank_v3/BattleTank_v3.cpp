a4c9c1b2a24ddb633f843d8c50a443ded6e5f0f0
Alex <ajwentzel@outlook.com>
2017-06-09 19:17:39 -0500

    Battleground level created

---
+++ BattleTank_v3/Source/BattleTank_v3/BattleTank_v3.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank_v3.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BattleTank_v3, "BattleTank_v3" );
---
+++ BattleTank_v3/Source/BattleTank_v3/BattleTank_v3GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleTank_v3GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_V3_API ABattleTank_v3GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
